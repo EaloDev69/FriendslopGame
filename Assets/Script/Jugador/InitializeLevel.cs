0bf88806843bde4faec3fbec3bcca1b4e2582ca1
Tom4S007 <tomas.bejarano@uao.edu.co>
2026-09-13 18:38:57 -0500

    Spawneo y movimiento resueltos en Niveles

--- Assets/Script/Jugador/InitializeLevel.cs
+++ Assets/Script/Jugador/InitializeLevel.cs
@@ -2,19 +2,26 @@ using UnityEngine;
 
 public class InitializeLevel : MonoBehaviour
 {
-    [Header("Insertar Players y prefabs")]
+    [Header("Insertar Players")]
     [SerializeField] private Transform[] playerSpawns;
-    [SerializeField] private GameObject[] playerPrefab;
 
     void Start()
     {
-        var playerConfigs = PlayerConfigurationManager.Instance.GetPlayerConfigs().ToArray();
-        for (int i = 0; i < playerConfigs.Length; i++)
+        var playerConfigs = PlayerConfigurationManager.Instance.GetPlayerConfigs();
+        for (int i = 0; i < playerConfigs.Count; i++)
         {
-            var player = Instantiate(playerPrefab[i], playerSpawns[i].position, playerSpawns[i].rotation, gameObject.transform);
-            player.GetComponent<PlayerInputHandler>().InitializePlayer(playerConfigs[i]);
+            var pc = playerConfigs[i];
 
+            // Usamos el objeto que YA existe y persistio desde el menu de seleccion
+            // (el que tiene el PlayerInput real, ya emparejado con su dispositivo),
+            // en vez de instanciar uno nuevo.
+            var playerObject = pc.Input.gameObject;
+
+            playerObject.transform.SetParent(gameObject.transform);
+            playerObject.transform.position = playerSpawns[i].position;
+            playerObject.transform.rotation = playerSpawns[i].rotation;
+
+            playerObject.GetComponent<PlayerInputHandler>().InitializePlayer(pc);
         }
-
     }
 }
--- Assets/Script/Jugador/PlayerInputHandler.cs
+++ Assets/Script/Jugador/PlayerInputHandler.cs
@@ -1,24 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputHandler : MonoBehaviour
 {
     PlayerConfigurations playerConfig;
 
-    private PlayerController controls;
-
     private GameObject classInstance;
+    private PlayerInput playerInput;
 
     void Awake()
     {
-        controls = new PlayerController();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     public void InitializePlayer(PlayerConfigurations pc)
     {
         playerConfig = pc;
-        if(pc.ClassPrefab != null)
+        if (pc.ClassPrefab == null) return;
+
+        classInstance = Instantiate(pc.ClassPrefab, transform);
+        classInstance.transform.localPosition = Vector3.zero;
+        classInstance.transform.localRotation = Quaternion.identity;
+
+        var controller = classInstance.GetComponent<PlayerController>();
+        if (controller != null && playerInput != null)
         {
-            classInstance = Instantiate(pc.ClassPrefab, transform.position, transform.rotation);
+            playerInput.actions["Gameplay/Move"].performed += controller.Move;
+            playerInput.actions["Gameplay/Move"].canceled += controller.Move;
+            playerInput.actions["Gameplay/Jump"].performed += controller.Jump;
+            playerInput.actions["Gameplay/Jump"].canceled += controller.Jump;
+
+            playerInput.SwitchCurrentActionMap("Gameplay"); // <- clave
         }
     }
 }
